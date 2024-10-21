d723bd79191e4b332c71a3a1f0bf59d2c474453e
sabrinaames <161710406+sabrinaames@users.noreply.github.com>
2024-10-21 10:21:40 -0300

    099

---
+++ js perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+`programação`,
+`O que é Python?`,
+
+)
+
+criaCartao(
+    `Geografia`,
+    `Qual a capital da França?`,
+    `A capital da França é paris`
+)
+
+criaCartao(
+    `Programação`,
+    `O que é uma função`,
+    `Ùma função é um bloco de código que executa alguma tarefa`
+)
+
+criaCartao(
+    `Lingua inglesa`,
+    `Como se diz oi em Ingles?`,
+    `Oi em ingles é HI (RAI)`
+)
